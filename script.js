93fcfae47a32af0012a6a5fe6534a1904fad1e09
drez <dreztech.pxl@gmail.com>
2019-07-26 21:40:45 -0600

    load txt

--- script.js
+++ script.js
@@ -2,21 +2,11 @@ $(document).ready(function(){
     // pull random quote into var
     // put to H1
 
-    // $.get('drezquotes.txt', function(txt) {
-    //     var lines = txt.responseText.split("\n");
-    //     var randLineNum = Math.floor(Math.random() * lines.length);
-    //     $("#drezquote").text(lines[randLineNum]); 
-    // });
+    $.get('drezquotes.txt', function(txt) {
+        var lines = txt.responseText.split("\n");
+        var randLineNum = Math.floor(Math.random() * lines.length);
+        $("#drezquote").text(lines[randLineNum]); 
+    });
 
-    // $.get('drezquotes.txt', function(data) {
-    //     var quotes = data.split("\n");
-    //     var idx = Math.floor(quotes.length * Math.random());
-    //     $('#drezquote').html(quotes[idx]);
-    // });
-
-   // $("h1").text(randomQuote( "drezquotes.txt"));
-
-  //  $("#drezquote").click(function() { });
-  $("#drezquote").load("drezquotes.txt");
 
 });
