9f68ba8ced88c019bc7a35993347fa87c79d186d
drez <dreztech.pxl@gmail.com>
2019-07-26 21:46:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-    // pull random quote into var
-    // put to H1
-
+    
     $.get('drezquotes.txt', function(txt) {
         var lines = txt.responseText.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
-        $("#drezquote").text(lines[randLineNum]); 
+        $("#drezquote").text(lines[randLineNum]); // random line from the text file
     });
-
 
 });
