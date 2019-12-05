ea3a7cc69dbabe7cbbeb80371e03c26aca8c6cff
drez <drez@outlook.com>
2019-12-04 21:44:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     var colors = ['c1','c2','c3','c4','c5'];
     var randColorNum = Math.floor(Math.random() * colors.length);
-    $("#wrapper").removeClass().addClass(colors[randColorNum]);
+    // $("#wrapper").removeClass().addClass(colors[randColorNum]); // set random color background
 
     $.get('drezquotes.txt', function(txt) {
         var lines = txt.split("\n");
