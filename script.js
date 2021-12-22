7a28d700b4acfdcd4a872f2b5e26e065cd105bc4
dreztech <dreztech.pxl@gmail.com>
2021-12-22 14:50:11 -0500

    added quotes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    var colors = ['c1','c2','c3','c4','c5'];
-    var randColorNum = Math.floor(Math.random() * colors.length);
+    //var colors = ['c1','c2','c3','c4','c5'];
+    //var randColorNum = Math.floor(Math.random() * colors.length);
     // $("#wrapper").removeClass().addClass(colors[randColorNum]); // set random color background
     $.get('drezquotes.txt', function(txt) {
-        var lines = txt.split("\n");
+        var lines = txt.split('\n');
         var randLineNum = Math.floor(Math.random() * lines.length);
-        ("#drezquote").text(lines[randLineNum]); // random line from the text file
+        console.log(lines[randLineNum]);
+        $('#drezquote').text(lines[randLineNum]); // random line from the text file
     });
 });
