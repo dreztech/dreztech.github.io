3738c03d9ca7ea8c731305c43104a9906994b9da
dreztech <dreztech.pxl@gmail.com>
2021-12-21 20:44:04 -0500

    added footer

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-
     var colors = ['c1','c2','c3','c4','c5'];
     var randColorNum = Math.floor(Math.random() * colors.length);
     // $("#wrapper").removeClass().addClass(colors[randColorNum]); // set random color background
-
     $.get('drezquotes.txt', function(txt) {
         var lines = txt.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
       // ("#drezquote").text(lines[randLineNum]); // random line from the text file
-
     });
-
 });
